b4f714f95964778a8b6d80217bd1ec9e5566de4e
Helder Pereira <phelder@gmail.com>
2023-08-30 20:56:13 +0200

    pokemon anme

--- code/v16-router-intro/src/views/HomeView.vue
+++ code/v16-router-intro/src/views/HomeView.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
     <h1>WELCOME TO THE HOMEPAGE</h1>
+    <ul>
+      <li v-for="pokemon in pokemonList" :key="pokemon.url">
+        <RouterLink :to="`/details?name=${pokemon.name}`">{{ pokemon.name }}</RouterLink>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      pokemonList: []
+    }
+  },
+  async created() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+    const result = await response.json()
+
+    this.pokemonList = result.results
+  }
+}
 </script>
 
 <style scoped></style>
